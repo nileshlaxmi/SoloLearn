e8468d5cf79b1529a6d451b06164577306d105d5
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-13 08:19:53 +0530

    Added css and updated algorithm

---
+++ sort-a-string/js/app.js
@@ -0,0 +1,22 @@
+/*
+const sortString = (str) => {
+    let ar = str.split("").sort().join("");
+    console.log(ar);
+}
+
+sortString("challenge");
+sortString("happy42");
+sortString("$tar3");
+*/
+
+
+function removeSpace(){
+    var inputData = $("#ipText").val();
+    var output = inputData.split("").sort().join("");
+    $("#opText").text(output);
+}
+
+function clearFields(){
+    document.getElementById('ipText').value = "";
+    document.getElementById('opText').innerHTML = "";
+}
