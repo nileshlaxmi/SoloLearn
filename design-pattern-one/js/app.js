4a8dc60596f766b4aac80a181bbf8b6b8c8e424d
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-14 20:47:10 +0530

    added design-pattern-one

---
+++ design-pattern-one/js/app.js
@@ -0,0 +1,12 @@
+const design = (n) => {
+    let i, j;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            console.log(i);
+        }
+        console.log("\n");
+    }
+}
+
+design(5);
