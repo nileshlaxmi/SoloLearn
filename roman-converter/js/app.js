6cc9c28f1812ff5c47c3d1aca5f96f4a83344a76
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-08-15 22:50:35 +0530

    Completed Roman Number Converter

--- roman-converter/js/app.js
+++ roman-converter/js/app.js
@@ -6,92 +6,83 @@ const romanConverter = (num) => {
     let ar = num.toString().split("");
 
     let len = ar.length;
+    let index = decimalArray.indexOf(num)
 
-    if(num === 1000){
-        indexA = decimalArray.indexOf(num);
-        output = romanArray[indexA];
-    }
-
-    if(len > 3){
-        d = parseInt(ar[0]);
-        a = parseInt(ar[1]) * 100;
-        b = parseInt(ar[2]) * 10;
-        c = parseInt(ar[3]);
-
-        let str = "";
-        for(let i=0; i<d; i++){
-            str = str + "M";
-        }
-        
-        if(a > 0){
-            indexA = decimalArray.indexOf(a);
-        }
-
-        if(b > 0){
-            indexB = decimalArray.indexOf(b);
-        }
-        
-        indexA = decimalArray.indexOf(a);
-        indexB = decimalArray.indexOf(b);
-        indexC = decimalArray.indexOf(c);
-
-        // console.log(indexA+ " " + indexB + " " + indexC);
-
-        if(indexA === -1 && indexB === -1){
-            output = str+romanArray[indexC];
-        } 
-        else if(indexA === -1 && indexB != -1){
-            output = str+romanArray[indexB]+romanArray[indexC];
-        } 
-        else if(indexB === -1 && indexA !== -1){
-            output = str+romanArray[indexA]+romanArray[indexC];
-        }
-        else if (indexB !== -1 || indexA !== -1){
-            output = str+romanArray[indexA]+romanArray[indexB]+romanArray[indexC];
-        }
-    }
-    else if(len <= 3 && len > 2) {
-        a = parseInt(ar[0]) * 100;
-        b = parseInt(ar[1]) * 10;
-        c = parseInt(ar[2]);
-
-        indexA = decimalArray.indexOf(a);
-
-        if(b !== 0){
-            indexB = decimalArray.indexOf(b);
-        }
-
-        if(c !== 0){
-            indexC = decimalArray.indexOf(c);
-        }
-
-        if(indexA !== -1 && indexB !== -1 && indexC !== -1){
-            output = romanArray[indexA]+romanArray[indexB]+romanArray[indexC];
-        }
-        else if(indexA !== -1 && indexB === -1 && indexC === -1){
-            output = romanArray[indexA];
-        } 
-        else if(indexA !== -1 && indexB === -1 && indexC !== -1){
-            output = romanArray[indexA]+romanArray[indexC];
-        } 
-        else if(indexA !== -1 && indexB !== -1 && indexC === -1){
-            output = romanArray[indexA]+romanArray[indexB];
-        }
-
-    }
-    else if(len <= 2 && len > 1) {
-        a = parseInt(ar[0]) * 10;
-        b = parseInt(ar[1]);
-
-        indexA = decimalArray.indexOf(a);
-        indexB = decimalArray.indexOf(b);
-        output = romanArray[indexA]+romanArray[indexB];
+    if(index !== -1){
+        output = romanArray[index];
     }
     else{
-        indexA = decimalArray.indexOf(parseInt(ar[0]));
-        output = romanArray[indexA];
-    }
+        if(len > 3){
+            d = parseInt(ar[0]);
+            a = parseInt(ar[1]) * 100;
+            b = parseInt(ar[2]) * 10;
+            c = parseInt(ar[3]);
     
+            let str = "";
+            for(let i=0; i<d; i++){
+                str = str + "M";
+            }
+            
+            if(a > 0){
+                indexA = decimalArray.indexOf(a);
+            }
+    
+            if(b > 0){
+                indexB = decimalArray.indexOf(b);
+            }
+            
+            indexA = decimalArray.indexOf(a);
+            indexB = decimalArray.indexOf(b);
+            indexC = decimalArray.indexOf(c);
+    
+            if(indexA === -1 && indexB === -1){
+                output = str+romanArray[indexC];
+            } 
+            else if(indexA === -1 && indexB != -1){
+                output = str+romanArray[indexB]+romanArray[indexC];
+            } 
+            else if(indexB === -1 && indexA !== -1){
+                output = str+romanArray[indexA]+romanArray[indexC];
+            }
+            else if (indexB !== -1 && indexA !== -1){
+                output = str+romanArray[indexA]+romanArray[indexB]+romanArray[indexC];
+            }
+        }
+        else if(len <= 3 && len > 2) {
+            a = parseInt(ar[0]) * 100;
+            b = parseInt(ar[1]) * 10;
+            c = parseInt(ar[2]);
+    
+            indexA = decimalArray.indexOf(a);
+    
+            if(b > 0){
+                indexB = decimalArray.indexOf(b);
+            }
+    
+            if(c > 0){
+                indexC = decimalArray.indexOf(c);
+            }
+    
+            if(indexB === undefined){
+                output = romanArray[indexA]+romanArray[indexC];
+            } 
+            else if(indexC === undefined){
+                output = romanArray[indexA]+romanArray[indexB];
+            }
+            else{
+                output = romanArray[indexA]+romanArray[indexB]+romanArray[indexC];
+            }
+    
+        }
+        else if(len <= 2 && len > 1) {
+            a = parseInt(ar[0]) * 10;
+            b = parseInt(ar[1]);
+    
+            indexA = decimalArray.indexOf(a);
+            indexB = decimalArray.indexOf(b);
+            output = romanArray[indexA]+romanArray[indexB];
+        }
+    }    
     console.log(output);
 }
 
