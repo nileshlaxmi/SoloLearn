a2b8f8a655aa13ed9ffd1983586a1d0e17e3f282
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2018-10-02 23:47:13 +0530

    Updated logic

--- disarium-number/js/app.js
+++ disarium-number/js/app.js
@@ -1,7 +1,22 @@
 const clickFunction = () => {
     var str = $("#ipText").val();
     console.log(str);
-    $("#opText").text(str);
+
+    let ar = str.split("");
+    let sum = 0;
+
+    for(let i=0; i<ar.length; i++){
+        let num = parseInt(ar[i]);
+        sum = sum + Math.pow(num, (i+1));
+    }
+
+    if(sum === parseInt(str)){
+        $("#opText").text("Disarium Number");
+    }
+    else{
+        $("#opText").text("Not a Disarium Number");
+    }
+    
 }
 
 function clearFields(){
